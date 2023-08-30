2bbe01c4cccd313fef0e00381e5eff66e3de802b
Amirali Amanipour <arshia.ala80@gmail.com>
2023-08-30 17:32:39 +0330

    fixed a bug style on the slider of the single post

--- components/slider/blog-other-posts/index.jsx
+++ components/slider/blog-other-posts/index.jsx
@@ -53,11 +53,35 @@ const BlogOtherPost = () => {
          view_number: 120,
          slug: 'laptop1',
       },
+      {
+         image: '/images/posts/tablet1.jpg',
+         title: 'لورم ایپسوم متن ساختگی ',
+         short_desc: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است ',
+         comments_number: 10,
+         view_number: 120,
+         slug: 'laptop1',
+      },
+      {
+         image: '/images/posts/tablet1.jpg',
+         title: 'لورم ایپسوم متن ساختگی ',
+         short_desc: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است ',
+         comments_number: 10,
+         view_number: 120,
+         slug: 'laptop1',
+      },
+      {
+         image: '/images/posts/tablet1.jpg',
+         title: 'لورم ایپسوم متن ساختگی ',
+         short_desc: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است ',
+         comments_number: 10,
+         view_number: 120,
+         slug: 'laptop1',
+      },
    ];
 
 
    return (
-      <div className=" container mx-auto">
+      <div className=" container mx-auto " >
          <section className="flex flex-col gap-4 px-2">
             <header className=" flex justify-between items-center">
                <h2 className=" text-orange-500 text-2xl border-r-orange-400 border-r-2 pr-1">
@@ -68,8 +92,8 @@ const BlogOtherPost = () => {
                   <FaChevronLeft onClick={() => { carouselSwitcher(-1) }} className=" cursor-pointer bg-zinc-200 transition-all duration-300 hover:bg-orange-400 w-10 h-10 p-2 rounded" />
                </div>
             </header>
-            <div ref={carouselRef} className="sliderContainer h-[24rem] max-w-[50rem] w-full  overflow-x-scroll px-4">
-               <div className=" flex justify-between items-center gap-8 ">
+            <div ref={carouselRef} className=" sliderContainer h-[24rem] max-w-[94rem] w-full overflow-x-scroll px-4">
+               <div className="sliderItem flex justify-between items-center gap-8 ">
                   {
                      posts.map((item,index)=><BlogMainBox key={index} data={item}/>)
                   }
