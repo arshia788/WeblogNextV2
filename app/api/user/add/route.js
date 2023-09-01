5504d0a63ee610651b62cc6fadbe5b2ee96e1a93
Amirali Amanipour <arshia.ala80@gmail.com>
2023-09-01 18:28:55 +0330

    worked on signup user for the datas and setted the tokents

--- app/api/user/add/route.js
+++ app/api/user/add/route.js
@@ -1,6 +1,11 @@
-import User from "@/models/User";
 import connect from "@/utils/connect";
 import { NextResponse } from "next/server";
+import User from "@/models/User";
+import bcrypt from 'bcrypt';
+
+import {cookies} from 'next/headers';
+
+import  Jwt  from "jsonwebtoken";
 
 
 export async function POST(req){
@@ -10,6 +15,7 @@ export async function POST(req){
         const inputData= await req.json();
         
 
+        // length_validations.
         if(inputData.username.length <8 || inputData.username.length > 20){
             return NextResponse.json({data:"نام کاربری باید بین 8 تا 20 کارکتر باشد"}, {status:402});
         }
@@ -37,9 +43,117 @@ export async function POST(req){
             return NextResponse.json({data:"نام کاربری باید بین 8 تا 24 کارکتر باشد"}, {status:402});
         }
 
+        // Unique_validations 
+
+        // nabyad mesle in ha bashe jai 
+        blog_name,username,phone
+
+        const phoneFound= await User.findOne({phone:inputData.phone});
+        if(phoneFound){
+            return NextResponse.json(
+                {data:'شما قبلا در سایت ثبت نام کرده اید.'},
+                {status:402}
+            )
+        }
+
+        const usernameFound= await User.findOne({username:inputData.username});
+        if(usernameFound){
+            return NextResponse.json(
+                {data:'  لطفا نام کاربری دیگری انتخاب بکنید'},
+                {status:402}
+            )
+        }
+
+        const blog_nameFound= await User.findOne({blog_name:inputData.blog_name});
+        if(blog_nameFound){
+            return NextResponse.json(
+                {data:'  لطفا نام وبلاگ دیگری انتخاب بکنید'},
+                {status:402}
+            )
+        }
+
+
         // For username spaces must convert dash 
+        // for pass spaces should be deleted 
+
+        // inja miad replace mikoneh space ha ro ba -
+        const newUsername= inputData.username.replace(/\s+/g,'-').toLowerCase();
+
+        // string khali yani miad hazfeh mikoneh on ro 
+
+        // newPassword baray in gofti newPassword ta hata on space ha ham rafteh bashan. 
+        const newPassword= inputData.password.replace(/\s+/g,'').toLowerCase();
+
+
+        // bcryptJs password  
+        const hashedPassword= bcrypt.hash(newPassword, 10);
+
+        // Making_active_code.
+
+        function generateRandoNumber(n){
+            return Math.floor(Math.pow(10, n-1) + (Math.random()* 9* Math.pow(10, n-1)))
+        }
+
+        const active_code= generateRandoNumber(6);
         
-        return NextResponse.json({data:users},{status:200});
+
+        // CREATIN_USER
+        const userFullData={
+
+            blog_name:inputData.blog_name,
+            username:newUsername,
+            display_name:inputData.display_name,
+            password:hashedPassword,
+            phone:inputData.phone,
+            createdAt: "1403/3/3" ,
+            default_image:`https://avatars.dicebear.com/api/bottts/${newUsername}.svg`,
+
+            // kasi ke signup kardan 3 hast 
+            // onai ke signup nakardan 4 hastan 
+
+            role:3,
+            active_code:active_code,
+            active_code_number:5,
+            user_is_active:false,
+
+            // in ro mizari false ta admin biad motavageh besheh.
+            viewed:false,
+
+            liked_posts:[],
+            bookmarked_posts:[],
+            followings:[],
+            notifications:[],
+            token:"",
+        }
+         
+        const createdUserData= await User.create(userFullData);
+
+        // JWT-TOKEN.
+
+        // miay inja token ro misazi 
+        const createdToken= Jwt.sign({_id:createdUserData._id, username:createdUserData.username}, process.env.TOKEN_SECRET);
+
+        const userToken= {
+            token:createdToken
+        };
+
+        await User.findByIdAndUpdate(createdUserData._id, userToken, {new:true});
+
+        // SETTING TOKEIN IN COOKIE
+        const cookieStore= cookies();
+        cookieStore.set("token", createdToken);
+
+        // DATA TO FRONT
+
+        const send_data={
+            userloged:true,
+            role:3,
+
+            // chon tazeh sabt nam kardeh
+            user_is_active:false,
+        }
+
+        return NextResponse.json({data:send_data, message:' ثبت نام با موفقیت انجام شد'},{status:200});
 
         
     } catch (error) {
--- models/User.js
+++ models/User.js
@@ -22,18 +22,17 @@ const userSchema= new Schema({
     ,phone:{
         type:String,
         required:true
-    },
-    phone:{
-        type:String,
-        required:true
     }
     ,
     createdAt:{
         type:String,
         required:true
-    },
+    }
+    ,
 
-    image:{
+    // in mishe vaseh mogehi ke dareh signup mikoneh va be sorat random ye img barash dar nazar gerefteh misheh. 
+    
+    default_image:{
         type:String,
         required:true
     }
@@ -42,45 +41,72 @@ const userSchema= new Schema({
     ,role:{
         type:Number,
         required:true,
-        // enum yani faght mitoneh bain hamin add ha bashe na meghdar digehi
+
         enum:[1,2,3,4],
         default:3
     }
     ,
+
+    // in active code vase taid karbar hast. 
+
     active_code:{
         type:Number,
         required:true
-    },
+    }
+    ,
+
+    // 
+    active_code_number:{
+        type:Number,
+        required:true,
+        default:5
+    }
+    ,
+
     user_is_active:{
         type:Boolean,
         required:true,
         default:false,
     },
+
+    // in vase admin hast 
     viewed:{
         type:Boolean,
         required:true,
         default:false,
     },
+
+    // kar bari ke hast che post hai ro liked kard. 
     liked_posts:{
         type:Array,
         required:true,
         default:[]
     },
+
+    // in ham mesle liked hast faght inja bookmark mishan. 
+    // * in like o bookmark ye array daran ke daronesh id on like ha ya bookmark ha hast.
+
     bookmarked_posts:{
         type:Array,
         required:true,
         default:[]
     },
+
+    // in ham ya array dareh ke daronesh id on kar bar hai hast ke omadeh follow kardeh. 
     followings:{
         type:Array,
         required:true,
         default:[]
     },
+
+    // 
     notifications:{
         type:Array,
         required:true,
         default:[]
     },
+
+    // in mishe hamoon token ahraz hovayati karbar 
     token:{
         type:String,
         required:true,
@@ -90,4 +116,4 @@ const userSchema= new Schema({
 })
 
 
-export default models.User || model("User", userSchema)
+export default models.User || model("User", userSchema);
